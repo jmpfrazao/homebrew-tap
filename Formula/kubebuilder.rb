ad673688d5d0bc221826115a73c7f2e910be9faf
knight42 <anonymousknight96@gmail.com>
2019-09-12 16:46:41 +0800

    chore: update kubebuilder

--- Formula/kubebuilder.rb
+++ Formula/kubebuilder.rb
@@ -3,18 +3,13 @@ class Kubebuilder < Formula
   homepage "https://github.com/kubernetes-sigs/kubebuilder"
   head "https://github.com/kubernetes-sigs/kubebuilder.git"
   version "2.0.0"
-
-  if OS.mac?
-    url "https://github.com/kubernetes-sigs/kubebuilder/releases/download/v2.0.0/kubebuilder_2.0.0_darwin_amd64.tar.gz"
-    sha256 "4679b5d57c857a0d3c763e9eefb4c797804a184fcae7827a3ca9abc990accc70"
-  elsif OS.linux?
-    url "https://github.com/kubernetes-sigs/kubebuilder/releases/download/v2.0.0/kubebuilder_2.0.0_linux_amd64.tar.gz"
-    sha256 "858d84aa3e8bb6528d7dd4dbab4e8fceb59c8ea7905918bc72dc719d784c40f3"
-  end
+  url "https://github.com/kubernetes-sigs/kubebuilder/releases/download/v2.0.0/kubebuilder_2.0.0_darwin_amd64.tar.gz"
+  sha256 "4679b5d57c857a0d3c763e9eefb4c797804a184fcae7827a3ca9abc990accc70"
 
   depends_on "kustomize"
 
   def install
     bin.install "bin/kubebuilder"
+    (prefix/"kubebuilder/bin").install "bin/etcd", "bin/kube-apiserver"
   end
 end
