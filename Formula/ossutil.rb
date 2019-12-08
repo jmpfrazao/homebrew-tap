30115f344b1f17c56cddfa0d210c4bf58dfe94aa
knight42 <anonymousknight96@gmail.com>
2019-12-08 12:14:15 +0800

    feat: upgrade ossutil to 1.6.9

--- Formula/ossutil.rb
+++ Formula/ossutil.rb
@@ -1,10 +1,10 @@
 class Ossutil < Formula
   desc "User friendly command-line tool to access AliCloud OSS"
-  version "1.6.7"
+  version "1.6.9"
   homepage "https://www.aliyun.com/product/oss"
   head "https://github.com/aliyun/ossutil.git"
   url "http://gosspublic.alicdn.com/ossutil/#{version}/ossutilmac64"
-  sha256 "5feb2edb8b341254b22f148d37311ef1b5408e1383be763dbb8f246f5634eb63"
+  sha256 "733941d0477094e42d34c6022fe492acb94042360efa071243f12f67585ae28d"
 
   def install
     bin.install "ossutilmac64"
