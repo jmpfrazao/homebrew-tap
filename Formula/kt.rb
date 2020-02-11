222d1abc6a52cf4beaa5cd79e437f3580a67c9bf
knight42 <anonymousknight96@gmail.com>
2020-02-11 13:49:01 +0800

    chore: bump kt to 0.0.2

--- Formula/kt.rb
+++ Formula/kt.rb
@@ -1,9 +1,9 @@
 class Kt < Formula
   homepage "https://github.com/knight42/kt"
-  version "0.0.1"
+  version "0.0.2"
   head "https://github.com/knight/kt.git"
   url "https://github.com/knight42/kt/releases/download/v#{version}/kt-v#{version}-darwin-amd64.tar.gz"
-  sha256 "4363d01b99435d37d16fc4dbe383116c01215924fbfe19a9d54574bbf85ea68c"
+  sha256 "a03b266bd8874981a19638f7904c1aeecf3d289ee910146fa84287ea0adef784"
 
   def install
     bin.install "kt"
