c0ade813da8b42fc9e2a74335570ae408b6a45a8
knight42 <anonymousknight96@gmail.com>
2020-02-17 15:28:21 +0800

    feat: upgrade kt to v0.0.3-alpha

--- Formula/kt.rb
+++ Formula/kt.rb
@@ -1,9 +1,9 @@
 class Kt < Formula
   homepage "https://github.com/knight42/kt"
-  version "0.0.2"
+  version "0.0.3-alpha"
   head "https://github.com/knight/kt.git"
   url "https://github.com/knight42/kt/releases/download/v#{version}/kt-v#{version}-darwin-amd64.tar.gz"
-  sha256 "a03b266bd8874981a19638f7904c1aeecf3d289ee910146fa84287ea0adef784"
+  sha256 "7e8d9689501f71d45bc567ff8120b36c891706e30bcf12040e9e1dbf9bd645b8"
 
   def install
     bin.install "kt"
