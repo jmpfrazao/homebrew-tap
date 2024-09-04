a89084c117cf0aba54250ffbdc78c12dfc4656fa
Jian Zeng <anonymousknight96@gmail.com>
2024-09-04 11:13:12 +0800

    feat: upgrade krelay to v0.1.1
    Signed-off-by: Jian Zeng <anonymousknight96@gmail.com>

--- Formula/krelay.rb
+++ Formula/krelay.rb
@@ -1,15 +1,15 @@
 class Krelay < Formula
   homepage "https://github.com/knight42/krelay"
-  version "0.0.10"
+  version "0.1.1"
   head "https://github.com/knight42/krelay.git"
   on_macos do
     on_arm do
       url "https://github.com/knight42/krelay/releases/download/v#{version}/kubectl-relay_v#{version}_darwin-arm64.tar.gz"
-      sha256 "d42c38b19bce929eed4902fdd32e76f0d7033a5d7324745ca5117e310c84f3c4"
+      sha256 "419b5d208e387e7d71ed06847cf2330a78e40d7f1bac322969c14eecb14671ac"
     end
     on_intel do
       url "https://github.com/knight42/krelay/releases/download/v#{version}/kubectl-relay_v#{version}_darwin-amd64.tar.gz"
-      sha256 "dcc4af52b8f647a7718127b11382943b71a0c888662b04807fb0c2bc33c92ad9"
+      sha256 "2ca64c5413ec4385ddbd93d60fbbf2407b7d9de3898023c583586dd175ba2836"
     end
   end
 
